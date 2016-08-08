21b6d451f5ea39e6cdaa9eebdf6815a1edd1ebbf
Tomo Masakura <tomo.masakura@gmail.com>
2016-08-08 21:54:56 +0900

    顔トレース

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,9 +18,31 @@ const app = (() => {
 $(document).ready(() => {
   'use strict';
 
+  const $video = $('#video');
+  const context = canvas.getContext('2d');
+  const $canvas = $('#canvas');
+  const ctracker = new clm.tracker();
+  ctracker.init(pModel);
+
+  $video.on('play', () => {
+    $video[0].width = $canvas[0].width = $video.width();
+    $video[0].height = $canvas[0].height = $video.height();
+
+    ctracker.start($video[0]);
+
+    drawLoop();
+  });
+
   app.getUserMedia({video: true, audio: false})
     .then(mediaStream => {
       video.src = URL.createObjectURL(mediaStream);
-    })
-    .catch(console.log);
+      return mediaStream;
+    });
+
+  function drawLoop() {
+    requestAnimationFrame(drawLoop);
+
+    context.clearRect(0, 0, $canvas[0].width, $canvas[0].height);
+    ctracker.draw(canvas);
+  }
 });
