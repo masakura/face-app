c22d1c4a1cac7581e77377a42472cf24f918a02d
Tomo Masakura <tomo.masakura@gmail.com>
2016-08-08 20:29:36 +0900

    カメラを表示

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('\'Allo \'Allo!');
+const app = (() => {
+  'use strict';
+
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+  function getUserMedia(constraints) {
+    return new Promise((resolve, reject) => {
+      navigator.getUserMedia(constraints, resolve, reject);
+    });
+  }
+
+  return {
+    getUserMedia
+  };
+
+})();
+
+$(document).ready(() => {
+  'use strict';
+
+  app.getUserMedia({video: true, audio: false})
+    .then(mediaStream => {
+      video.src = URL.createObjectURL(mediaStream);
+    })
+    .catch(console.log);
+});
